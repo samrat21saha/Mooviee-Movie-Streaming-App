9ec2ee27c9ce11ac98ee1760179b34328e0a0f97
Samrat Saha <samratsaha9635@gmail.com>
2025-12-05 23:54:00 +0530

    added api base url for fetch data for get api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React, { useState } from "react";
+import React, {useEffect, useState } from "react";
 import Search from "./components/Search";
 
 // TMDB_API_KEY_TOKEN= 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMGJlMzhmM2M0YmI5NjFjMDFjZjkzODQzNTU1NmI2NCIsIm5iZiI6MTc2NDgzNzk5My42MjUsInN1YiI6IjY5MzE0YTY5MjRhMmZmY2NiNGYyNDZmYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Kll5klwP68rePgx46f7oyoeZOip1w0VWUWSzMu0jdqA'
 
 // API_KEY = 'c0be38f3c4bb961c01cf938435556b64'
+
+const API_BASE_URL= "https://api.themoviedb.org/3";
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
