69b5da5a76177c86547ce4a35bf72f1e0dffef82
Samrat Saha <samratsaha9635@gmail.com>
2025-12-09 12:47:39 +0530

    package.json and package-lock.json updated in the git repo

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { useDebounce } from "react-use";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
-const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_KEY = import.meta.env.VITE_TMDB_TOKEN;
 
 const API_OPTIONS = {
   method: 'GET',
